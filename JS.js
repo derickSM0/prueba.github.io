7c1ec79c56cd52bc93c0dabbcc8a29676d329853
derickSM0 <129110097+derickSM0@users.noreply.github.com>
2023-05-15 15:10:06 -0500

    main

--- JS.js
+++ JS.js
@@ -1,5 +1,4 @@
 var year=prompt('Cual es tu year de nacimiento?');
-var yactual= 2023
-year=parseInt(year)
-var yeardenacimiento=2023 - year
+year=parseInt(year);
+var yeardenacimiento=2023 - year;
 document.write("Tu edad es ", yeardenacimiento);
